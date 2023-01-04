01363281b6fa990056e419a4b5e4e49885dcddf4
99corn <hahahoho5915@gmail.com>
2023-01-04 10:30:49 +0900

    #15 Case문
    - 기본 case when then 문

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -471,4 +471,21 @@ public class QuerydslBasicTest {
             System.out.println("t = " + t);
         }
     }
+
+    @Test
+    public void basicCase() {
+        QMember member = QMember.member;
+
+        List<String> result = queryFactory
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
 }
