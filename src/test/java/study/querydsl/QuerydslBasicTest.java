d39296470e2d1d4f2debb7747f7fd65ff7b565a6
99corn <hahahoho5915@gmail.com>
2023-01-04 10:33:10 +0900

    #15 Case문
    - 복잡한 case 문 -> CaseBuilder() 사용

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
@@ -481,6 +482,24 @@ public class QuerydslBasicTest {
                         .when(10).then("열살")
                         .when(20).then("스무살")
                         .otherwise("기타"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void complexCase() {
+        QMember member = QMember.member;
+
+        List<String> result = queryFactory
+                .select(new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살")
+                        .when(member.age.between(21, 30)).then("21~30살")
+                        .otherwise("기타")
+                )
                 .from(member)
                 .fetch();
 
